a3e0cbe4f281e6ead7846578ed9473eeb2aec45d
y4ngyy <591620892@qq.com>
2022-05-10 05:49:30 -0700

    initialize

---
+++ init.sh
@@ -0,0 +1,16 @@
+# assume git was installed
+sudo cp ./sources.list /etc/apt/
+sudo apt-get update
+# install vm tool
+# sudo apt-get install open-vm-tools open-vm-tools-desktop -y
+# install common software
+sudo apt-get install vim -y
+sudo apt-get install terminator openssh-server net-tools -y
+# remove unuse software
+sudo apt-get purge libreoffice-common -y
+sudo apt-get purge thunderbird totem rhythmbox simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-sudoku -y
+sudo apt-get autoremove
+# init workspace
+mkdir work
+# init terminator
+cp ./terminator_config ~/.config/terminator/config
