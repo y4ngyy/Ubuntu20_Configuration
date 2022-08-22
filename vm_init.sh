bf35d64f9ca8e62423aa96e0dfdbfebfbd0c656c
y4ngyy <591620892@qq.com>
2022-08-22 11:32:19 +0800

    Update vm_init.sh

--- vm_init.sh
+++ vm_init.sh
@@ -9,10 +9,11 @@ sudo apt-get install terminator openssh-server net-tools -y
 # remove unuse software
 sudo apt-get purge libreoffice-common -y
 sudo apt-get purge thunderbird totem rhythmbox simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-sudoku -y
-sudo apt-get autoremove
-# init workspace
-mkdir work
+sudo apt-get autoremove -y
 # init terminator
+if [ ! -f "~/.config/terminator" ]; then
+  mkdir ~/.config/terminator
+fi
 cp ./terminator_config ~/.config/terminator/config
 # init vimrc
 cp ./vim_config ~/.vimrc
